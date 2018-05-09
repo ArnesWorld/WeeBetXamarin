ca15451b62a33cd9576f2a9d1c5fc2e4e4751d27
jitka753 <jitka.polaskova753@gmail.com>
2018-05-09 10:50:59 +0200

    tennis

---
+++ WeeBet.Android/View/ShowTennisView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Droid.Views;
+using WeeBet.Core.ViewModels;
+
+namespace WeeBet.Android.View
+{
+    [Activity(Label = "ShowTennisView", MainLauncher = false)]
+    public class ShowTennisView : MvxActivity<ShowTennisViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            ShowTennisViewModel model = (ShowTennisViewModel)ViewModel;
+
+            // Create your application here
+        }
+        protected override void OnViewModelSet()
+        {
+            SetContentView(Resource.Layout.ShowTennisView);
+        }
+    }
+}
---
+++ WeeBet.Core/ViewModels/ShowTennisViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeeBet.Core.ViewModels
+{
+    class ShowTennisViewModel
+    {
+    }
+}
