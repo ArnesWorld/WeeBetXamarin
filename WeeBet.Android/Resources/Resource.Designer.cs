afb8b64e407a8a40a36fea93603bedcef5402ec1
jitka753 <jitka.polaskova753@gmail.com>
2018-05-19 19:55:24 +0200

    icon

--- WeeBet.Android/Resources/Resource.Designer.cs
+++ WeeBet.Android/Resources/Resource.Designer.cs
@@ -100,16 +100,19 @@ namespace WeeBet.Android
 			public const int hockey = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int pic2 = 2130837507;
+			public const int icon = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int soccer = 2130837508;
+			public const int pic2 = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int swimming = 2130837509;
+			public const int soccer = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int tennis = 2130837510;
+			public const int swimming = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int tennis = 2130837511;
 			
 			static Drawable()
 			{
